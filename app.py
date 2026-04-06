1b879337530dc97a8300e59062e21e81fd26d96a
Jordan Baumann <0501jordanb@gmail.com>
2026-04-06 09:26:54 -0400

    Code Cleanup and Prep for Part E

---
+++ app.py
@@ -0,0 +1,20 @@
+from dataload import *
+
+conn, cursor = open_connection()
+
+load_lockers(cursor)
+load_doctors(cursor)
+load_patients(cursor)
+load_appointments(cursor)
+load_patient_doctors(cursor)
+
+SAVE_RESULTS = True
+
+if SAVE_RESULTS:
+    save_lockers(conn)
+    save_doctors(conn)
+    save_appointments(conn)
+    save_patients(conn)
+    save_patient_doctors(conn)
+
+close_connection(conn, cursor)
--- dataload.py
+++ dataload.py
@@ -3,7 +3,7 @@ import pandas as pd
 import pyarrow
 
 
-def fetch_data(table_name: str):
+def fetch_data(table_name: str, conn: oracledb.Connection):
     return pyarrow.table(conn.fetch_df_all(
         "SELECT * FROM " + table_name)).to_pandas()
 
@@ -14,87 +14,111 @@ DB_USER = "system"
 DB_PASS = "GoofyGoober25"
 DB_DSN = "localhost:1521/xe"
 
-if LIB_DIR:
-    oracledb.init_oracle_client(lib_dir=LIB_DIR)
-else:
-    oracledb.enable_thin_mode()
 
-try:
-    conn = oracledb.connect(user=DB_USER, password=DB_PASS, dsn=DB_DSN)
-except:
-    print("Failed to initialize connection!")
+def open_connection() -> tuple[oracledb.Connection, oracledb.Cursor]:
+    if LIB_DIR:
+        oracledb.init_oracle_client(lib_dir=LIB_DIR)
+    else:
+        oracledb.enable_thin_mode()
 
-try:
-    cursor = conn.cursor()
-except:
-    print("Failed to initialize cursor!")
+    try:
+        conn = oracledb.connect(user=DB_USER, password=DB_PASS, dsn=DB_DSN)
+    except:
+        print("Failed to initialize connection!")
 
-
-appointment_data = pd.read_csv("data/appointment.csv")
-doctor_data = pd.read_csv("data/doctor.csv")
-locker_data = pd.read_csv("data/locker.csv")
-patient_data = pd.read_csv("data/patient.csv")
-patient_doctor_data = pd.read_csv("data/patient_doctor.csv")
+    try:
+        cursor = conn.cursor()
+    except:
+        print("Failed to initialize cursor!")
+    return conn, cursor
 
 
-appointment_sql = "INSERT INTO Appointment\
-(appointment_id, patient_id, scheduled_day, appointment_day,\
-sms_received, no_show, doctor_id) VALUES \
-(:1, :2, :3, :4, :5, :6, :7)"
-
-doctor_sql = "INSERT INTO Doctor\
-(doctor_id, locker_id) VALUES (:1, :2)"
-
-locker_sql = "INSERT INTO Locker\
-(locker_id, user_id, user_type) VALUES (:1, :2, :3)"
-
-patient_sql = "INSERT INTO Patient\
-(patient_id, gender, age, neighbourhood, scholarship, hypertension,\
-diabetes, alcoholism, handicap, locker_id) VALUES \
-(:1, :2, :3, :4, :5, :6, :7, :8, :9, :10)"
-
-patient_doctor_sql = "INSERT INTO Patient_Doctor\
-(patient_id, doctor_id) VALUES (:1, :2)"
+def close_connection(conn, cursor):
+    cursor.close()
+    conn.close()
+    print("Oracle connection closed.")
 
 
-for (locker_id, user_id, user_type) in locker_data.itertuples(index=False):
-    cursor.execute(locker_sql, [locker_id, user_id, user_type])
+def load_lockers(cursor):
+    data = pd.read_csv("data/locker.csv")
 
-for (doctor_id, locker_id) in doctor_data.itertuples(index=False):
-    cursor.execute(doctor_sql, [doctor_id, None if locker_id == -1 else locker_id])
+    sql = "INSERT INTO Locker "\
+        "(locker_id, user_id, user_type) VALUES (:1, :2, :3)"
 
-for (patient_id, gender, age, neighbourhood, scholarship, hypertension,\
-    diabetes, alcoholism, handicap, locker_id) in patient_data.itertuples(index=False):
-    cursor.execute(patient_sql, [patient_id, gender, age, neighbourhood,\
-        scholarship, hypertension, diabetes, alcoholism, handicap,\
-        None if locker_id == -1 else locker_id])
-
-for (appointment_id, patient_id, scheduled_day, appointment_day,\
-    sms_received, no_show, doctor_id) in appointment_data.itertuples(index=False):
-    cursor.execute(appointment_sql, [appointment_id, patient_id,\
-        scheduled_day, appointment_day, sms_received, no_show,\
-        None if doctor_id == -1 else doctor_id])
-
-for (patient_id, doctor_id) in patient_doctor_data.itertuples(index=False):
-    cursor.execute(patient_doctor_sql, [patient_id, doctor_id])
+    for (locker_id, user_id, user_type) in data.itertuples(index=False):
+        cursor.execute(sql, [locker_id, user_id, user_type])
 
 
+def load_doctors(cursor):
+    data = pd.read_csv("data/doctor.csv")
 
-SAVE_RESULTS = True
-if SAVE_RESULTS:
-    appointment_data = fetch_data("Appointment")
-    doctor_data = fetch_data("Doctor")
-    locker_data = fetch_data("Locker")
-    patient_data = fetch_data("Patient")
-    patient_doctor_data = fetch_data("Patient_Doctor")
+    sql = "INSERT INTO Doctor "\
+        "(doctor_id, locker_id) VALUES (:1, :2)"
 
-    appointment_data.to_csv("results/appointment.csv", index=False)
-    doctor_data.to_csv("results/doctor.csv", index=False)
-    locker_data.to_csv("results/locker.csv", index=False)
-    patient_data.to_csv("results/patient.csv", index=False)
-    patient_doctor_data.to_csv("results/patient_doctor.csv", index=False)
+    for (doctor_id, locker_id) in data.itertuples(index=False):
+        cursor.execute(sql, [doctor_id, None if locker_id == -1 else locker_id])
+        
+
+def load_appointments(cursor):
+    data = pd.read_csv("data/appointment.csv")
+    
+    sql = "INSERT INTO Appointment "\
+        "(appointment_id, patient_id, scheduled_day, appointment_day, "\
+        "sms_received, no_show, doctor_id) VALUES "\
+        "(:1, :2, :3, :4, :5, :6, :7)"
+    
+    for (appointment_id, patient_id, scheduled_day, appointment_day,\
+        sms_received, no_show, doctor_id) in data.itertuples(index=False):
+        cursor.execute(sql, [appointment_id, patient_id,\
+            scheduled_day, appointment_day, sms_received, no_show,\
+            None if doctor_id == -1 else doctor_id])
 
 
-cursor.close()
-conn.close()
-print("Oracle connection closed.")
+def load_patients(cursor):
+    data = pd.read_csv("data/patient.csv")
+
+    sql = "INSERT INTO Patient "\
+        "(patient_id, gender, age, neighbourhood, scholarship, hypertension, "\
+        "diabetes, alcoholism, handicap, locker_id) VALUES "\
+        "(:1, :2, :3, :4, :5, :6, :7, :8, :9, :10)"
+        
+    for (patient_id, gender, age, neighbourhood, scholarship, hypertension,\
+        diabetes, alcoholism, handicap, locker_id) in data.itertuples(index=False):
+        cursor.execute(sql, [patient_id, gender, age, neighbourhood,\
+            scholarship, hypertension, diabetes, alcoholism, handicap,\
+            None if locker_id == -1 else locker_id])
+    
+    
+def load_patient_doctors(cursor):
+    data = pd.read_csv("data/patient_doctor.csv")
+
+    sql = "INSERT INTO Patient_Doctor "\
+        "(patient_id, doctor_id) VALUES (:1, :2)"
+
+    for (patient_id, doctor_id) in data.itertuples(index=False):
+        cursor.execute(sql, [patient_id, doctor_id])
+        
+
+def save_lockers(conn):
+    fetch_data("Locker", conn)\
+        .to_csv("results/locker.csv", index=False)
+    
+    
+def save_doctors(conn):
+    fetch_data("Doctor", conn)\
+        .to_csv("results/doctor.csv", index=False)
+
+
+def save_appointments(conn):
+    fetch_data("Appointment", conn)\
+        .to_csv("results/appointment.csv", index=False)
+
+
+def save_patients(conn):
+    fetch_data("Patient", conn)\
+        .to_csv("results/patient.csv", index=False)
+
+
+def save_patient_doctors(conn):
+    fetch_data("Patient_Doctor", conn)\
+        .to_csv("results/patient_doctor.csv", index=False)
