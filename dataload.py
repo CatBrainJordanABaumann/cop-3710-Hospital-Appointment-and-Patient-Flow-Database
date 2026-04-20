46c235ba71b2ad48d974b8bf2c7d69ff65b48b25
Jordan Baumann <bbaumann2194@floridapoly.edu>
2026-04-20 09:04:28 -0400

    Fixed Password Input
    Used to ask for a password without a space after the colon making it look weird. Such has now been fixed and input for username and password is standardized.

--- dataload.py
+++ dataload.py
@@ -11,7 +11,7 @@ def fetch_data(table_name: str, conn: oracledb.Connection):
 
 LIB_DIR = r"C:\oracle\instantclient_11_2"
 DB_USER = input("Username: ")
-DB_PASS = input("Password:")
+DB_PASS = input("Password: ")
 DB_DSN = "localhost:1521/xe"
 
 
